4d1366bfa08bd648078742c898f7b88d6d9d1022
Ilya <i.veroniys@gmail.com>
2026-05-06 00:05:11 +0300

    updates

--- js/script.js
+++ js/script.js
@@ -21,6 +21,20 @@ navLinks.forEach(link => {
 });
 
 
+// Плаўны скрол да якараў на старонцы
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop - 60,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
 
 // слайдэр для галоўнага блоку
 
@@ -106,16 +120,3 @@ document.getElementById("modalImg").onclick = function() {
 
 
 
-// Плаўны скрол да якараў на старонцы
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        if (target) {
-            window.scrollTo({
-                top: target.offsetTop - 60,
-                behavior: 'smooth'
-            });
-        }
-    });
-});
